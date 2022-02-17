455cac58e614f2f4c773b45fb2ec0c54eccc86c9
Sallie McKee <salliemckee@Morrigan.local>
2022-02-17 16:29:35 -0600

    finished styling

--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,15 +1,43 @@
 import React from "react";
-import axios from "axios";
-import ReactPlayer from "react-player";
+import styled, { keyframes } from "styled-components";
+
+const kf = keyframes`
+50% {
+  opacity: 0;
+}
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
+
+const StyledNasaPhoto = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #abdbe3;
+  font-family: "Cairo", sans-serif;
+
+  .image {
+    max-width: 70%;
+    padding: 10px;
+    border: 3px solid black;
+
+    animation: ${kf} 0.5s ease-in-out backwards;
+  }
+  .explanation {
+    max-width: 70%;
+  }
+`;
 
 const NasaPhoto = (props) => {
   return (
-    <div className="nasa-photo-container">
+    <StyledNasaPhoto className="nasa-photo-container">
       <h2>{props.photo.title}</h2>
       <h3>{props.photo.date}</h3>
-      <ReactPlayer url={props.photo.url} className="video-player" />
+      <img src={props.photo.hdurl} className="image" />
       <p className="explanation">{props.photo.explanation}</p>
-    </div>
+    </StyledNasaPhoto>
   );
 };
 
---
+++ src/theme/index.js
@@ -0,0 +1,4 @@
+export default {
+  primaryColor: "#abdbe3",
+  secondaryColor: "black",
+};
